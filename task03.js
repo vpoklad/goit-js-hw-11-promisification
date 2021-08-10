67d133d837cbf0d24f2cd1126436d4bb2a2af499
Viktor Poklad <3d.poklad@gmail.com>
2021-08-10 23:40:57 +0300

    Update task03.js

--- task03.js
+++ task03.js
@@ -2,21 +2,25 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction, onSuccess, onError) => {
+const makeTransaction = (transaction) => {
   const delay = randomIntegerFromInterval(200, 500);
+    return new Promise((resolve, reject) => {
 
-  setTimeout(() => {
-    const canProcess = Math.random() > 0.3;
+    setTimeout(() => {
+        const canProcess = Math.random() > 0.3;
+        
 
     if (canProcess) {
-      onSuccess(transaction.id, delay);
+        resolve({ id:transaction.id, time: delay });
     } else {
-      onError(transaction.id);
+      reject(transaction.id);
     }
   }, delay);
+})
+  
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
@@ -27,25 +31,25 @@ const logError = id => {
 /*
  * Работает так
  */
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
 /*
  * Должно работать так
  */
-// makeTransaction({ id: 70, amount: 150 })
-//   .then(logSuccess)
-//   .catch(logError);
+makeTransaction({ id: 70, amount: 150 })
+  .then(logSuccess)
+  .catch(logError);
 
-// makeTransaction({ id: 71, amount: 230 })
-//   .then(logSuccess)
-//   .catch(logError);
+makeTransaction({ id: 71, amount: 230 })
+  .then(logSuccess)
+  .catch(logError);
 
-// makeTransaction({ id: 72, amount: 75 })
-//   .then(logSuccess)
-//   .catch(logError);
+makeTransaction({ id: 72, amount: 75 })
+  .then(logSuccess)
+  .catch(logError);
 
-// makeTransaction({ id: 73, amount: 100 })
-//   .then(logSuccess)
-//   .catch(logError);
+makeTransaction({ id: 73, amount: 100 })
+  .then(logSuccess)
+  .catch(logError);
